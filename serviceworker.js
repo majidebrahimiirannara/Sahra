bdaab9eea796c425f78a1d7830a2d3c6e79da124
majidebrahimiirannara <qom.irannara@gmail.com>
2020-07-02 19:32:23 +0430

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,33 @@
+var cacheName = 'SahraSW';
+var appShellFiles = [
+  '/',
+  '/index.html',
+  '/13990408ResetSession.css',
+  '/jquery-2.2.4.min.js',
+  '/favicon.ico',
+  '/wwb12.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
